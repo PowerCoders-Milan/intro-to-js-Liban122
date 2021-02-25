e466afa5c40b1206caa99bb74c5c15f4e1dfaec0
liban <botanlee515@gmail.com>
2021-02-25 14:31:41 +0100

    Done

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,2 +1,22 @@
 // Example
 window.alert("Hello World!");
+
+var name = prompt('Enter your name');
+alert("Hello " + name) ;
+var ask = prompt('How are you today ' + name + ' ?');
+var good = 'good'
+var bad = 'bad'
+
+if(ask === good){
+alert('Im happy for you ' + name)
+}
+
+else if(ask === bad) {
+    alert('That is too bad ' + name + ' tomorrow its a new day' )
+
+}
+
+
+
+
+
