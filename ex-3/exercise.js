7ea3e19b63883395b4d3d6393fdfb0d2574d24a7
liban <botanlee515@gmail.com>
2021-02-25 13:40:59 +0100

    Done

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,17 @@
 // Example
 window.alert("Hello World!");
+var x = 9;
+var y = 15;
+if (x > y){
+    console.log('X is greater than Y');
+    
+}
+else if (x < y) {
+    console.log('Y is greater than X')
+}
+
+else{
+    console.log('X and Y are Equal')
+}
+
+
