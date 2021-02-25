92c56188fe906e2c94cb36da442e3823a715307e
liban <botanlee515@gmail.com>
2021-02-25 13:08:44 +0100

    Done

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,11 @@
 // Example
 window.alert("Hello World!");
+
+var numberOfChildren = 4;
+var father = 'wiley';
+var mother = 'eli'
+var geographicLocation = 'India';
+var jobTitle = 'Enginer'
+
+console.log('You will be an ' + jobTitle + ' in ' + geographicLocation + ' You will be married to ' + mother + ' and you will have ' + numberOfChildren )
+
