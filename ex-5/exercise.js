3558b6d5f2c3fe015163a77f7681e7aa02e78cbf
liban <botanlee515@gmail.com>
2021-02-25 16:15:58 +0100

    Done

--- ex-5/exercise.js
+++ ex-5/exercise.js
@@ -1,2 +1,18 @@
 // Example
 window.alert("Hello World!");
+ var x = prompt('Enter your String')
+ alert('Your String length is ' +  x.length + ' Characters')
+ alert(' Now lets compare 2 strings length')
+ var y = prompt('Please Enter your 1st string')
+ var z = prompt('Please Enter your 2nd String')
+
+ if(y.length > z.length){
+     alert('Your 1st string is greater have ' + y.length + ' Characters' + ' and the 2nd string length is ' + z.length)
+ }
+ else if (y.length < z.length) {
+    alert('Your 2nd string is greater and it have ' + z.length + ' Chrarcters' + ' and the 1st string length is ' + y.length)
+
+ }
+
+
+ 
